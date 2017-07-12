939b7d632d57a1b8feff09ff4e41b8e61bf5c4c9
yumingzhou <yumingzhoutyut@163.com>
2017-07-12 09:43:32 +0800

    ok
    dubbo-zookeeper，分布式服务框架的提供者端

---
+++ provider/src/main/java/com/yu/service/TestService.java
@@ -0,0 +1,8 @@
+package com.yu.service;
+
+/**
+ * Created by ymz_pc on 2017/7/11.
+ */
+public interface TestService {
+    public String getName();
+}
---
+++ provider/src/main/java/com/yu/service/impl/TestServiceImpl.java
@@ -0,0 +1,11 @@
+package com.yu.service.impl;
+
+import com.yu.service.TestService;
+/**
+ * Created by ymz_pc on 2017/7/11.
+ */
+public class TestServiceImpl implements TestService{
+    public String getName(){
+        return "yumingzhou provide this message for you";
+    }
+}
